2499b6b32ee0f2da2182ca1b943b0886e1d69509
tamam <tamam.assalimi@gmail.com>
2019-09-15 14:33:53 +0700

    code refactor

---
+++ config/mysqlConfig.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'B1smillah92',
+    database: 'crud_db'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Mysql Connected...');
+    console.log(connection.threadId);
+})
+
+module.exports = {connection};
---
+++ controller/mysql/productController.js
@@ -0,0 +1,43 @@
+const {connection} = require('../../config/mysqlConfig.js');
+
+function getAllProduct(req, res) {
+    let sql = "SELECT * FROM product";
+    let query = connection.query(sql, (err, results) => {
+        if (err) throw err;
+        res.render('product_view', {
+            results: results
+        });
+    });
+}
+
+function insertProduct(req, res){
+    let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+    let sql = "INSERT INTO product SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.redirect('/product');
+    });
+}
+
+function updateProduct(req,res){
+    let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
+    executeQuery(sql,req,res);
+}
+
+function deleteProduct(req,res){
+    let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+    executeQuery(sql,req,res);
+}
+
+function executeQuery(sql,req,res){
+    let query = connection.query(sql, (err, results) => {
+        if(err) throw err;
+            res.redirect('/product');
+    });
+}
+module.exports = {
+    getAllProduct,
+    insertProduct,
+    updateProduct,
+    deleteProduct
+};
